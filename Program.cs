b70aecbc8251bc324a5eca2fc2c4e9cac2c915a6
“Adalmiina <adalmiinasuikkanen3@gmail.com>
2023-05-25 21:13:57 +0300

    Task: changing the folder structure

--- Program.cs
+++ Program.cs
@@ -43,14 +43,11 @@ namespace HelsinkiBikes
                 app.UseSwaggerUI();
             }
 
-
-
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = string.Empty;
             });
-
 
             app.UseHttpsRedirection();
 
@@ -67,7 +64,7 @@ namespace HelsinkiBikes
             /* UNCOMMENT THESE WHEN YOU RUN THE FIRST TIME. AND COMMENT AGAIN AFTER*/
 
             //importTrips.seeding("data/2021-05.csv");
-            importTrips.seeding("data/2021-06.csv");
+            //importTrips.seeding("data/2021-06.csv");
             //importTrips.seeding("data/2021-07.csv");
 
             //importStations.seeding("data/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat_avoin.csv");
